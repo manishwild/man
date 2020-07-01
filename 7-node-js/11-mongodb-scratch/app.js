6875199859c6651972075ede1b263bbb0c0c3233
manish-dci <manishwild1000@gmail.com>
2020-07-01 16:02:54 +0200

    hhj

--- 7-node-js/11-mongodb-scratch/app.js
+++ 7-node-js/11-mongodb-scratch/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+//const MongoClient = require('mongodb')
+//const ObjectID = require('mongodb')
+//destructuring mongodb
+const { MongoClient, ObjectID } = require('mongodb');
 const { response } = require('express');
 
 const app = express()
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 const connectionString = 'mongodb+srv://fbw5:123456abc@cluster0.wtcsm.mongodb.net/test1?retryWrites=true&w=majority'
 
 app.get('/', (req, res) => {
@@ -74,16 +81,16 @@ app.get('/insertmany', (req, res) => {
         const response = await db.collection('users').insertMany([
             {
             email:'man@m1an.com',
-            password:'123456'
+            password:'1235456'
         },{
             email:'man@ma1n.com',
-            password:'123456'
+            password:'1235456'
         },{
             email:'man@ma1n.com',
-            password:'123456'
+            password:'1235456'
         },{
             email:'man@ma1n.com',
-            password:'123456'
+            password:'1235456'
         }
     ])
         client.close() 
@@ -114,7 +121,113 @@ app.get('/getuser', (req, res) => {
         
     })()
 });
+app.get('/findone', (req, res) => {
+    (async()=>{
+        
+        try {
+            const client = await MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology:true})
+            const db = client.db('test1')
+            const response = await db.collection('users').findOne({_id:new ObjectID('5efc5d9fcc250a3a2a96c49c')})
+            client.close()
+            res.send(response)
+        } catch (error) {
+            res.send(error);
+        }
+        
+    })()
+});
+app.get('/updatemany', (req, res) => {
+    (async() =>{
+        try {
+            const client = await MongoClient.connect(connectionString,{useNewUrlParser:true,useUnifiedTopology:true})
+            const db = client.db('test1')
+            const response = await db.collection('users').updateMany({password:'123456'},{
+                $set:{email:'manis@man.com'}
+            })
+            client.close()
+            res.send(response);
+        } catch (error) {
+            res.send(error);
+        }
+    })()
+});
 
+app.get('/updateone', (req, res) => {
+    (async()=>{
+        try {
+            const client = await MongoClient.connect(connectionString,{useNewUrlParser:true,useUnifiedTopology:true})
+            const db =  client.db('test1')
+            const response = await db.collection('users').updateOne({_id:new ObjectID('5efc5d9fcc250a3a2a96c49c')},{
+                $set:{email:'mas@man.com'}
+            })
+            client.close()
+            res.send(response);
+        } catch (error) {
+            res.send(error)
+        }
+
+    })()
+});
+
+
+app.get('/deletemany', (req, res) => {
+    (async()=>{
+        try {
+            const client = await MongoClient.connect(connectionString,{useNewUrlParser:true,useUnifiedTopology:true})
+            const db =  client.db('test1')
+            const response = await db.collection('users').deleteMany({password:'123456'})
+            client.close()
+            res.send(response);
+        } catch (error) {
+            res.send(error)
+        }
+
+    })()
+});
+
+
+app.get('/deletone', (req, res) => {
+    (async()=>{
+        try {
+            const client = await MongoClient.connect(connectionString,{useNewUrlParser:true,useUnifiedTopology:true})
+            const db =  client.db('test1')
+            const response = await db.collection('users').deleteOne({_id:new ObjectID('5efc83a10922d73201b64410')})
+            client.close()
+            res.send(response);
+        } catch (error) {
+            res.send(error)
+        }
+
+    })()
+});
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+app.post('/register', (req, res) => {
+    console.log(req.body)
+    
+    console.log(userData);
+    (async()=>{
+        try {
+            const userData = {
+        username: req.body.username,
+        password: req.body.password
+    }
+           const client = await MongoClient.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology:true})
+        const db = client.db('test1')
+        const response = await db.collection('users').insertOne({
+            username: req.body.username,
+            password: req.body.password
+        })
+        client.close() 
+        res.send(response);
+        } catch (error) {
+            res.send(error);
+        }
+        
+        
+    })()
+});
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
